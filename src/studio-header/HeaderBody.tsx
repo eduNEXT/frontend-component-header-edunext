d712030cbeb11e7c427a856b9e0e02c67e128d4a
Andres Felipe Giraldo Malagon <andres.giraldo@edunext.co>
2025-04-02 09:32:43 -0500

    feat: add language selector component to header

--- src/studio-header/HeaderBody.tsx
+++ src/studio-header/HeaderBody.tsx
@@ -1,45 +1,24 @@
-import React, { type ReactNode, type ComponentProps } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
+import { getConfig } from '@edx/frontend-platform';
+import { useIntl } from '@edx/frontend-platform/i18n';
 import classNames from 'classnames';
 import {
   ActionRow,
   Button,
   Container,
+  Icon,
+  IconButton,
   Nav,
   Row,
 } from '@openedx/paragon';
-import { Close, MenuIcon } from '@openedx/paragon/icons';
-
+import { Close, MenuIcon, Search } from '@openedx/paragon/icons';
+import LanguageSelector from '../language-selector';
 import CourseLockUp from './CourseLockUp';
 import UserMenu from './UserMenu';
 import BrandNav from './BrandNav';
 import NavDropdownMenu from './NavDropdownMenu';
-import StudioHeaderSearchButtonSlot from '../plugin-slots/StudioHeaderSearchButtonSlot';
-
-export interface HeaderBodyProps {
-  studioBaseUrl: string;
-  logoutUrl: string;
-  setModalPopupTarget?: ((instance: HTMLButtonElement | null) => void) | null;
-  toggleModalPopup?: React.MouseEventHandler<HTMLButtonElement>;
-  isModalPopupOpen?: boolean;
-  number?: string;
-  org?: string;
-  title: string;
-  logo: string;
-  logoAltText: string;
-  authenticatedUserAvatar?: string;
-  username?: string;
-  isAdmin?: boolean;
-  isMobile?: boolean;
-  isHiddenMainMenu?: boolean;
-  mainMenuDropdowns?: {
-    id: string;
-    buttonTitle: ReactNode;
-    items: { title: ReactNode; href: string; }[];
-  }[];
-  outlineLink?: string;
-  searchButtonAction?: React.MouseEventHandler<HTMLButtonElement>;
-  containerProps?: Omit<ComponentProps<typeof Container>, 'children'>;
-}
+import messages from './messages';
 
 const HeaderBody = ({
   logo,
@@ -53,15 +32,16 @@ const HeaderBody = ({
   logoutUrl,
   authenticatedUserAvatar,
   isMobile,
-  setModalPopupTarget = null,
+  setModalPopupTarget,
   toggleModalPopup,
-  isModalPopupOpen = false,
-  isHiddenMainMenu = false,
-  mainMenuDropdowns = [],
+  isModalPopupOpen,
+  isHiddenMainMenu,
+  mainMenuDropdowns,
   outlineLink,
   searchButtonAction,
-  containerProps = {},
-}: HeaderBodyProps) => {
+  containerProps,
+}) => {
+  const intl = useIntl();
 
   const renderBrandNav = (
     <BrandNav
@@ -73,7 +53,7 @@ const HeaderBody = ({
     />
   );
 
-  const { className: containerClassName, ...restContainerProps } = containerProps;
+  const { className: containerClassName, ...restContainerProps } = containerProps || {};
 
   return (
     <Container
@@ -137,9 +117,18 @@ const HeaderBody = ({
           </>
         )}
         <ActionRow.Spacer />
-        <StudioHeaderSearchButtonSlot
-          searchButtonAction={searchButtonAction}
-        />
+        {getConfig().ENABLE_HEADER_LANG_SELECTOR && (<LanguageSelector />)}
+        {searchButtonAction && (
+          <Nav>
+            <IconButton
+              src={Search}
+              iconAs={Icon}
+              onClick={searchButtonAction}
+              aria-label={intl.formatMessage(messages['header.label.search.nav'])}
+              alt={intl.formatMessage(messages['header.label.search.nav'])}
+            />
+          </Nav>
+        )}
         <Nav>
           <UserMenu
             {...{
@@ -148,13 +137,61 @@ const HeaderBody = ({
               logoutUrl,
               authenticatedUserAvatar,
               isAdmin,
-              isMobile,
             }}
           />
         </Nav>
       </ActionRow>
     </Container>
   );
+};
+
+HeaderBody.propTypes = {
+  studioBaseUrl: PropTypes.string.isRequired,
+  logoutUrl: PropTypes.string.isRequired,
+  setModalPopupTarget: PropTypes.func,
+  toggleModalPopup: PropTypes.func,
+  isModalPopupOpen: PropTypes.bool,
+  number: PropTypes.string,
+  org: PropTypes.string,
+  title: PropTypes.string,
+  logo: PropTypes.string,
+  logoAltText: PropTypes.string,
+  authenticatedUserAvatar: PropTypes.string,
+  username: PropTypes.string,
+  isAdmin: PropTypes.bool,
+  isMobile: PropTypes.bool,
+  isHiddenMainMenu: PropTypes.bool,
+  mainMenuDropdowns: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.string,
+    buttonTitle: PropTypes.node,
+    items: PropTypes.arrayOf(PropTypes.shape({
+      href: PropTypes.string,
+      title: PropTypes.node,
+    })),
+  })),
+  outlineLink: PropTypes.string,
+  searchButtonAction: PropTypes.func,
+  containerProps: PropTypes.shape(Container.propTypes),
+};
+
+HeaderBody.defaultProps = {
+  setModalPopupTarget: null,
+  toggleModalPopup: null,
+  isModalPopupOpen: false,
+  logo: null,
+  logoAltText: null,
+  number: '',
+  org: '',
+  title: '',
+  authenticatedUserAvatar: null,
+  username: null,
+  isAdmin: false,
+  isMobile: false,
+  isHiddenMainMenu: false,
+  mainMenuDropdowns: [],
+  outlineLink: null,
+  searchButtonAction: null,
+  containerProps: {},
 };
 
 export default HeaderBody;
