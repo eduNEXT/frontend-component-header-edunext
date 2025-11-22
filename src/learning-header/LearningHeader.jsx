400eb95d68f9a20a5295dd33d0c48968e211cc03
Andres Espinel <andressespinel@hotmail.com>
2025-11-21 20:28:58 -0500

    fix: fixed React import

--- src/learning-header/LearningHeader.jsx
+++ src/learning-header/LearningHeader.jsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
 import { getConfig } from '@edx/frontend-platform';
 import { injectIntl, intlShape } from '@edx/frontend-platform/i18n';
